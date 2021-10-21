c08490c189e66e49fd7be55c4e5376982c6299ac
Portia Chen <portiachen81@gmail.com>
2021-10-22 00:04:43 +0800

    add a link for ContentLink page

--- src/components/ContentLink/ContentLink1.js
+++ src/components/ContentLink/ContentLink1.js
@@ -5,18 +5,86 @@ function ContentLink1() {
   return (
     <>
       <div className="linkAgainst1">
-        <img src="../../../images/LogoLink/Tbvs健康新聞.png" alt="" />
-        <img src="../../../images/LogoLink/Heho健康.png" alt="" />
-        <img src="../../../images/LogoLink/大紀元.png" alt="" />
-        <img src="../../../images/LogoLink/建康99.png" alt="" />
-        <img src="../../../images/LogoLink/衛生福利部.png" alt="" />
-        <img src="../../../images/LogoLink/早安健康新聞.png" alt="" />
-        <img src="../../../images/LogoLink/Tbvs健康新聞.png" alt="" />
-        <img src="../../../images/LogoLink/Heho健康.png" alt="" />
-        <img src="../../../images/LogoLink/大紀元.png" alt="" />
-        <img src="../../../images/LogoLink/建康99.png" alt="" />
-        <img src="../../../images/LogoLink/衛生福利部.png" alt="" />
-        <img src="../../../images/LogoLink/早安健康新聞.png" alt="" />
+        <a
+          className="changeLinkTo"
+          target="_blank"
+          href="https://news.tvbs.com.tw/health"
+        >
+          <img
+            src="../../../images/LogoLink/Tbvs健康新聞.png"
+            alt="Tbvs健康新聞"
+          />
+        </a>
+        <a className="changeLinkTo" target="_blank" href="https://heho.com.tw/">
+          <img src="../../../images/LogoLink/Heho健康.png" alt="Heho健康" />
+        </a>
+        <a
+          className="changeLinkTo"
+          target="_blank"
+          href="https://www.epochtimes.com/b5/"
+        >
+          <img src="../../../images/LogoLink/大紀元.png" alt="大紀元" />
+        </a>
+        <a
+          className="changeLinkTo"
+          target="_blank"
+          href="https://health99.hpa.gov.tw/"
+        >
+          <img src="../../../images/LogoLink/建康99.png" alt="建康99" />
+        </a>
+        <a
+          className="changeLinkTo"
+          target="_blank"
+          href="https://www.mohw.gov.tw/mp-1.html"
+        >
+          <img src="../../../images/LogoLink/衛生福利部.png" alt="衛生福利部" />
+        </a>
+        <a className="changeLinkTo" target="_blank" href="https://www.edh.tw/">
+          <img
+            src="../../../images/LogoLink/早安健康新聞.png"
+            alt="早安健康新聞"
+          />
+        </a>
+        <a
+          className="changeLinkTo"
+          target="_blank"
+          href="https://news.tvbs.com.tw/health"
+        >
+          <img
+            src="../../../images/LogoLink/Tbvs健康新聞.png"
+            alt="Tbvs健康新聞"
+          />
+        </a>
+        <a className="changeLinkTo" target="_blank" href="https://heho.com.tw/">
+          <img src="../../../images/LogoLink/Heho健康.png" alt="Heho健康" />
+        </a>
+        <a
+          className="changeLinkTo"
+          target="_blank"
+          href="https://www.epochtimes.com/b5/"
+        >
+          <img src="../../../images/LogoLink/大紀元.png" alt="大紀元" />
+        </a>
+        <a
+          className="changeLinkTo"
+          target="_blank"
+          href="https://health99.hpa.gov.tw/"
+        >
+          <img src="../../../images/LogoLink/建康99.png" alt="建康99" />
+        </a>
+        <a
+          className="changeLinkTo"
+          target="_blank"
+          href="https://www.mohw.gov.tw/mp-1.html"
+        >
+          <img src="../../../images/LogoLink/衛生福利部.png" alt="衛生福利部" />
+        </a>
+        <a className="changeLinkTo" target="_blank" href="https://www.edh.tw/">
+          <img
+            src="../../../images/LogoLink/早安健康新聞.png"
+            alt="早安健康新聞"
+          />
+        </a>
       </div>
     </>
   );
--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -13,62 +13,101 @@ function Navbar() {
   const [buttonIndex, setButtonIndex] = useState(0);
   return (
     <>
-      <div className="navbarDesTop">
-        <div className="homeIconLogo">
-          <img src="../../../images/Logo.jpg" alt="" />
-        </div>
-        <div className="navbarLinkDesTop">
-          <div className="navbarLinkDesTop1">
-            <p className="positionIconA">
-              <MdOutlineAssignment />
-              <p>處方領藥</p>
-            </p>
-            <div className="navbarAllIcon">
-              <FiHeart />
-              <BsCart2 />
-              <FaRegUserCircle />
+      {/* onMouseLeave 滑鼠移出事件  這個navbarAll沒用到 */}
+      <div
+        className="navbarAll"
+        onMouseLeave={() => {
+          setButtonIndex(0);
+        }}
+      >
+        <div className="navbarDesTop">
+          <div className="homeIconLogo">
+            <img src="../../../images/Logo.jpg" alt="" />
+          </div>
+          <div className="navbarLinkDesTop">
+            <div className="navbarLinkDesTop1">
+              <p className="positionIconA">
+                <MdOutlineAssignment />
+                <p>處方領藥</p>
+              </p>
+              <div className="navbarAllIcon">
+                <FiHeart />
+                <BsCart2 />
+                <FaRegUserCircle />
+              </div>
+            </div>
+            <div className="navbarLinkDesTop2">
+              <a
+                href=""
+                onMouseEnter={() => {
+                  setButtonIndex(0);
+                }}
+              >
+                <p className="navbarIconLine1">線上購物</p>
+              </a>
+              <div className="forCenter">
+                <p
+                  className="navbarIconLine1"
+                  onMouseEnter={() => {
+                    setButtonIndex(1);
+                  }}
+                >
+                  保健知識
+                </p>
+                <img
+                  className={
+                    buttonIndex === 1
+                      ? "halfCircleMove"
+                      : "halfCircleMove hideClass"
+                  }
+                  src="../../../images/photo/halfcircle.png"
+                  alt="半圓"
+                />
+              </div>
+              <div>
+                <p
+                  className="navbarIconLine1"
+                  onMouseEnter={() => {
+                    setButtonIndex(2);
+                  }}
+                >
+                  消息專區
+                </p>
+                <img
+                  className={
+                    buttonIndex === 2
+                      ? "halfCircleMove"
+                      : "halfCircleMove hideClass"
+                  }
+                  src="../../../images/photo/halfcircle.png"
+                  alt="半圓"
+                />
+              </div>
+              <div>
+                <p
+                  onMouseEnter={() => {
+                    setButtonIndex(3);
+                  }}
+                  className="navbarIconLineAbouts"
+                >
+                  關於我們
+                </p>
+                <img
+                  className={
+                    buttonIndex === 3
+                      ? "halfCircleMove navbarIconLineAbouts"
+                      : "halfCircleMove hideClass"
+                  }
+                  src="../../../images/photo/halfcircle.png"
+                  alt="半圓"
+                />
+              </div>
             </div>
           </div>
-          <div
-            className="navbarLinkDesTop2"
-            onMouseEnter={() => {
-              setButtonIndex(0);
-            }}
-          >
-            <a href="">
-              <p className="navbarIconLine1">線上購物</p>
-            </a>
-
-            <p
-              className="navbarIconLine1"
-              onMouseEnter={() => {
-                setButtonIndex(1);
-              }}
-            >
-              保健知識
-            </p>
-
-            <p
-              className="navbarIconLine1"
-              onMouseEnter={() => {
-                setButtonIndex(2);
-              }}
-            >
-              消息專區
-            </p>
-
-            <p
-              onMouseEnter={() => {
-                setButtonIndex(3);
-              }}
-            >
-              關於我們
-            </p>
-          </div>
         </div>
-      </div>
-      <div className={buttonIndex !== 0 ? "buttonAndIconPush" : ""}>
-        {buttonIndex === 1 ? <NavbarPush /> : ""}
+        <div className={buttonIndex !== 0 ? "buttonAndIconPush" : ""}>
+          {buttonIndex === 1 ? <NavbarPush /> : ""}
+        </div>
       </div>
     </>
   );
